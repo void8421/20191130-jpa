e53eb0022f2b4de46efeabc08a8957151344f558
voidchar8421 <1044829344@qq.com>
2019-11-30 09:58:56 +0800

    jpa框架练习 项目架构搭建 springboot+jpa

---
+++ src/main/java/com/zhou/jpa/ApplicationRun.java
@@ -0,0 +1,18 @@
+package com.zhou.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author mac
+ * @ClassName ApplicationRun
+ * @Description
+ * @Date 2019/11/30 9:37 上午
+ */
+@SpringBootApplication
+public class ApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+    }
+}
